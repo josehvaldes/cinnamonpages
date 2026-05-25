ec90d37ceb985b8e89645633e2d947f2a359ed65
josehvaldes <jmurguia@gmail.com>
2026-05-25 13:43:05 -0400

    Add a basic under-construction page in react+vite

---
+++ src/App.tsx
@@ -0,0 +1,28 @@
+import { useState } from 'react'
+import underconstructionImg from './assets/under-construction.png'
+import './App.css'
+
+function App() {
+  const [count, setCount] = useState(0)
+
+  return (
+    <>
+      <section id="center">
+        <div className="hero">
+          <img src={underconstructionImg} alt="Under construction" />
+        </div>
+        
+        <button
+          type="button"
+          className="counter"
+          onClick={() => setCount((count) => count + 1)}
+        >
+          Count is {count}
+        </button>
+      </section>
+
+    </>
+  )
+}
+
+export default App
