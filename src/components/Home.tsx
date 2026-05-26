1d9a7c137cf38355618daf5842caa8813d299eda
josehvaldes <jmurguia@gmail.com>
2026-05-26 15:53:21 -0400

    Add Mantine components

---
+++ src/components/Home.tsx
@@ -0,0 +1,134 @@
+
+import { ProductCard } from './ProductCard';
+
+
+const products = [
+  {
+    img: 'https://images.unsplash.com/photo-1555982105-d25af4182e4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&h=400&q=80',
+    name: 'Product Name',
+    price: '£9.99',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1508423134147-addf71308178?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&h=400&q=80',
+    name: 'Product Name',
+    price: '£9.99',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1449247709967-d4461a6a6103?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&h=400&q=80',
+    name: 'Product Name',
+    price: '£9.99',
+  },
+  {
+    img: 'https://images.unsplash.com/reserve/LJIZlzHgQ7WPSh5KVTCB_Typewriter.jpg?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&h=400&q=80',
+    name: 'Product Name',
+    price: '£9.99',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1467949576168-6ce8e2df4e13?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&h=400&q=80',
+    name: 'Product Name',
+    price: '£9.99',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1544787219-7f47ccb76574?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&h=400&q=80',
+    name: 'Product Name',
+    price: '£9.99',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1550837368-6594235de85c?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=400&h=400&q=80',
+    name: 'Product Name',
+    price: '£9.99',
+  },
+  {
+    img: 'https://images.unsplash.com/photo-1551431009-a802eeec77b1?ixlib=rb-1.2.1&auto=format&fit=crop&w=400&h=400&q=80',
+    name: 'Product Name',
+    price: '£9.99',
+  },
+];
+
+interface HomeProps {
+
+}
+
+
+export function Home({}: HomeProps) {
+
+  return (
+    <div className='pb-20' >
+    <div className="carousel relative container mx-auto" style={{ maxWidth: '1600px' }}>
+        <div className="carousel-inner relative overflow-hidden w-full">
+          {/* Slide 1 */}
+          <div className="carousel-item absolute opacity-0" style={{ height: '50vh', opacity: 1, position: 'static' }}>
+            <div className="block h-full w-full mx-auto flex pt-6 md:pt-0 md:items-center bg-cover bg-right" style={{ backgroundImage: `url('https://images.unsplash.com/photo-1422190441165-ec2956dc9ecc?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=80')` }}>
+              <div className="container mx-auto">
+                <div className="flex flex-col w-full lg:w-1/2 md:ml-16 items-center md:items-start px-6 tracking-wide">
+                  <p className="text-black text-2xl my-4">Stripy Zig Zag Jigsaw Pillow and Duvet Set</p>
+                  <a className="text-xl inline-block no-underline border-b border-gray-600 leading-relaxed hover:text-black hover:border-black" href="#">view product</a>
+                </div>
+              </div>
+            </div>
+          </div>
+          <label htmlFor="carousel-3" className="prev control-1 w-10 h-10 ml-2 md:ml-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-gray-900 leading-tight text-center z-10 inset-y-0 left-0 my-auto">‹</label>
+          <label htmlFor="carousel-2" className="next control-1 w-10 h-10 mr-2 md:mr-10 absolute cursor-pointer hidden text-3xl font-bold text-black hover:text-white rounded-full bg-white hover:bg-gray-900 leading-tight text-center z-10 inset-y-0 right-0 my-auto">›</label>
+
+
+          {/* Slide 2 */}
+          <div className="carousel-item absolute opacity-0 bg-cover bg-right" style={{ height: '50vh' }}>
+            <div className="block h-full w-full mx-auto flex pt-6 md:pt-0 md:items-center bg-cover bg-right" style={{ backgroundImage: `url('https://images.unsplash.com/photo-1533090161767-e6ffed986c88?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjM0MTM2fQ&auto=format&fit=crop&w=1600&q=80')` }}>
+              <div className="container mx-auto">
+                <div className="flex flex-col w-full lg:w-1/2 md:ml-16 items-center md:items-start px-6 tracking-wide">
+                  <p className="text-black text-2xl my-4">Real Bamboo Wall Clock</p>
+                  <a className="text-xl inline-block no-underline border-b border-gray-600 leading-relaxed hover:text-black hover:border-black" href="#">view product</a>
+                </div>
+              </div>
+            </div>
+          </div>
+          {/* Slide 3 */}
+          <div className="carousel-item absolute opacity-0" style={{ height: '50vh' }}>
+            <div className="block h-full w-full mx-auto flex pt-6 md:pt-0 md:items-center bg-cover bg-bottom" style={{ backgroundImage: `url('https://images.unsplash.com/photo-1519327232521-1ea2c736d34d?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=crop&w=1600&q=80')` }}>
+              <div className="container mx-auto">
+                <div className="flex flex-col w-full lg:w-1/2 md:ml-16 items-center md:items-start px-6 tracking-wide">
+                  <p className="text-black text-2xl my-4">Brown and blue hardbound book</p>
+                  <a className="text-xl inline-block no-underline border-b border-gray-600 leading-relaxed hover:text-black hover:border-black" href="#">view product</a>
+                </div>
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+
+      <section className="bg-white py-8">
+        <div className="container mx-auto flex items-center flex-wrap pt-4 pb-12">
+          <nav id="store" className="w-full z-30 top-0 px-6 py-1">
+            <div className="w-full container mx-auto flex flex-wrap items-center justify-between mt-0 px-2 py-3">
+              <a className="uppercase tracking-wide no-underline hover:no-underline font-bold text-gray-800 text-xl " href="#">Store</a>
+              <div className="flex items-center" id="store-nav-content">
+                <a className="pl-3 inline-block no-underline hover:text-black" href="#">
+                  <svg className="fill-current hover:text-black" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                    <path d="M7 11H17V13H7zM4 7H20V9H4zM10 15H14V17H10z" />
+                  </svg>
+                </a>
+                <a className="pl-3 inline-block no-underline hover:text-black" href="#">
+                  <svg className="fill-current hover:text-black" xmlns="http://www.w3.org/2000/svg" width="24" height="24" viewBox="0 0 24 24">
+                    <path d="M10,18c1.846,0,3.543-0.635,4.897-1.688l4.396,4.396l1.414-1.414l-4.396-4.396C17.365,13.543,18,11.846,18,10 c0-4.411-3.589-8-8-8s-8,3.589-8,8S5.589,18,10,18z M10,4c3.309,0,6,2.691,6,6s-2.691,6-6,6s-6-2.691-6-6S6.691,4,10,4z" />
+                  </svg>
+                </a>
+              </div>
+            </div>
+          </nav>
+          {/* Product Cards */}
+          {products.map((product, idx) => (
+            <ProductCard key={idx} {...product} />
+          ))}
+        </div>
+      </section>
+
+      <section className="bg-white">
+        <div className="container pb-20 px-6 mx-auto">
+          <a className="uppercase tracking-wide no-underline hover:no-underline font-bold text-gray-800 text-xl mb-8" href="#">About</a>
+          <p className="mb-8">Lorem ipsum dolor sit amet, consectetur <a href="#">random link</a> adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Vel risus commodo viverra maecenas accumsan lacus vel facilisis volutpat. Vitae aliquet nec ullamcorper sit. Nullam eget felis eget nunc lobortis mattis aliquam. In est ante in nibh mauris. Egestas congue quisque egestas diam in. Facilisi nullam vehicula ipsum a arcu. Nec nam aliquam sem et tortor consequat. </p>
+        </div>
+      </section>
+      
+    </div>
+  );
+}
